14425b30f475740fe79b5649e33b035564fd0b28
José Henrique <jose.almeida10@escola.pr.gov.br>
2025-06-05 13:11:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Atacantes',
+    'Quais os três maiores artilheiros da história',
+    'Pelé, Lionel Messi e Cristiano Ronaldo'
+)
+
+criaCartao(
+    'Meia',
+    'Quais os três maiores meias da historia do futebol',
+    'Zico, Zinedine Zidane e Ronaldinho Gaúcho'
+)
+
+criaCartao(
+    'Meio campista',
+    'Quais os três maiores meio campista da historia do futebol',
+    'Modric, Xavi, Didi'
+)
+
+criaCartao(
+    'Zagueiros',
+    'Quais os três maiores zagueiros da história do futebol',
+    'Beckenbauer,Baresi e Maldini'
+)
+
+criaCartao(
+    'Goleiros',
+    'Quais os três maiores goleiros da história do futebol?',
+    'Lev Yashin, Gianluigi Buffon e Manuel Neuer'
+)
+
+criaCartao(
+    'Técnicos',
+    'Quais os três maiores técnicos da história do futebol?',
+    'Sir Alex Ferguson, Pep Guardiola e Johan Cruyff'
+)
+
